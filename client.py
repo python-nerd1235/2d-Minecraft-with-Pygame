668bc447276736974294ea65582aeabd2053f520
python-nerd1235 <calboyg93@gmail.com>
2026-06-01 14:52:26 -0400

    Update client.py to accept server IP input

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 # Mincraft 2D Online - Client
-#version 0.1 (game equivalent to 2dMincraft version 0.4, without saveing.)
+#version 0.1_A (game equivalent to 2dMincraft version 0.4, without saveing.)
 import Mincraft2DonlineLib
-c=Mincraft2DonlineLib.cli('localhost', 12345)
+c=Mincraft2DonlineLib.cli(input("Enter server IP: "), 12345)
 c.connect()
 c.run_client()
 print("Thanks for playing!")
