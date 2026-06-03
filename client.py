7c3fadb4662cf48ac740c15b474d59930ab31512
python-nerd1235 <calboyg93@gmail.com>
2026-06-03 12:58:11 -0400

    Delete client.py

--- client.py
+++
@@ -1,7 +0,0 @@
-# Mincraft 2D Online - Client
-#version 0.1_A (game equivalent to 2dMincraft version 0.4, without saveing.)
-import Mincraft2DonlineLib
-c=Mincraft2DonlineLib.cli(input("Enter server IP: "), 12345)
-c.connect()
-c.run_client()
-print("Thanks for playing!")
